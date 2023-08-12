4c625dca37b963076ededdfb6f38c71f032f8365
Tony Amir <amirtony57@gmail.com>
2023-08-12 17:48:29 +0300

    app

---
+++ Routes/countryRoutes.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const bodyParser = require('body-parser');
+
+const router= express.Router()
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+const {
+    createCountry,
+    viewCountries,
+    viewcountry
+}=require("../Controllers/countryController");
+
+
+router.get("/country/",viewCountries)
+router.get("/country/:Name",createCountry)
+router.post("/country",createCountry)
+
+module.exports=router
---
+++ Routes/teamRoutes.js
@@ -0,0 +1,29 @@
+const express=require('express')
+const bodyParser = require('body-parser');
+
+const router= express.Router()
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+const {
+    createTeam,
+    viewTeam,
+    viewTeams,
+    updateCoins,
+    updatePower,
+    addMembers,
+    assignCountry,
+    tradeCountry,
+    attendance
+}=require("../Controllers/teamController");
+
+
+router.get("/teams/",viewTeams)
+router.get("/teams/:Name",viewTeam)
+router.post("/teams",createTeam)
+router.put("/updateCoins",updateCoins)
+router.put("/updatePower",updatePower)
+router.put("/addMembers",addMembers)
+router.put("/assignCountry",assignCountry)
+router.put("/tradeCountry",tradeCountry)
+router.put("/attendance",attendance)
+module.exports=router
