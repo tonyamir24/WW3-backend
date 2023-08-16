b96edc807318b2af41bc654f5deb77d78f4dd54c
Tony Amir <amirtony57@gmail.com>
2023-08-16 19:17:02 +0300

    Qrrr

--- Controllers/challengeController.js
+++ Controllers/challengeController.js
@@ -5,16 +5,20 @@ const getChallenge = async (req, res) => {
   const { ID, TeamName } = req.body;
   try {
     const challenge = await Challenge.findOne({ ID });
-    if (challenge.Reward === true) {
-      if (challenge.Value > 0) {
-        const team = await Team.findOneAndUpdate(
-          { Name: TeamName },
-          { $inc: { Coins: challenge.Value } }
-        );
+    if (challenge.Reward) {
+      if (challenge.Reward === true) {
+        if (challenge.Value > 0) {
+          const team = await Team.findOneAndUpdate(
+            { Name: TeamName },
+            { $inc: { Coins: challenge.Value } }
+          );
+        }
+        await Challenge.findOneAndUpdate({ ID }, { Reward: false });
+      } else {
+        throw Error("Qr Scanned Before");
       }
-      await Challenge.findOneAndUpdate({ ID }, { Reward: false });
     } else {
-      throw Error("Qr Scanned Before");
+      throw Error("Not a Valid QR Code (Msh hata5od attendance mn hena)");
     }
 
     await res.status(200).json(challenge);
